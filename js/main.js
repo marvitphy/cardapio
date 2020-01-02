8628517b6e33c079c9e438fdf254d842f538cd3d
marvitphy <48634870+marvitphy@users.noreply.github.com>
2020-01-02 14:31:28 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,12 +88,6 @@ $(document).ready(function(){
       });
 
 
-      $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
-});
-
-
 
 
       
