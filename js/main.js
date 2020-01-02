9983a9b498a04d6e4eda65f8613933606b71ef3f
marvitphy <48634870+marvitphy@users.noreply.github.com>
2020-01-02 14:30:24 -0300

    asdf

--- js/main.js
+++ js/main.js
@@ -92,12 +92,9 @@ $(document).ready(function(){
 $(window).on('load', function(){
   setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
 });
-function removeLoader(){
-    $( "#loadingDiv" ).fadeOut(500, function() {
-      // fadeOut complete. Remove the loading div
-      $( "#loadingDiv" ).remove(); //makes page more lightweight 
-  });  
-}
+
+
+
 
       
 
