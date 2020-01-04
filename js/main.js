b817744fbe8d5c38ba87fee7c9ea5fcdabf4369d
marvitphy <48634870+marvitphy@users.noreply.github.com>
2020-01-04 17:00:43 -0300

    atualizações matinais

--- js/main.js
+++ js/main.js
@@ -1,54 +1,54 @@
 
-var var1;
+
+
 
 
 $(document).ready(function(){
 
     
-
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
-
-    $toggleCollapse.click(function(){
-
-        $nav.toggleClass('collapse');
-
-    });
+    
 
     
-    var var1;
-       
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+    $toggleCollapse.click(function(){
+
+        $nav.toggleClass('collapse'); 
+    });
+
 
     $( ".btn-acessar" ).click(function() {
         $(".cardapio").css({"transform": "translateX(0px)"});
       });
 
-      $( ".fechar" ).click(function() {
+    $( ".fechar" ).click(function() {
         $(".cardapio").css({"transform": "translateX(100%)"});
       });
 
       // FORMULÁRIO
-      
-      var $checkboxes = $('#ingredientes-container input[type="checkbox"]');
 
-        
+      var $checkboxes = $('#ingredientes-container input[type="checkbox"]');
       $checkboxes.change(function(){
-          var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
-          
-          if(countCheckedCheckboxes > 10){
-            
+          var countCheckedCheckboxes = $checkboxes.filter(':checked').length; 
+          if(countCheckedCheckboxes > 10){     
             $('#count-checked-checkboxes').text(countCheckedCheckboxes + " Ingrediente(s) Selecionado(s)");
             $("#count-checked-checkboxes").css({"color": "red"});
             $("#selected-cbx").css({"display": "none"});
             $("#aviso-ingredientes").css({"display": "initial"});
+            valoringrediente = (countCheckedCheckboxes - 10) * 2;    
         } 
+        else if((countCheckedCheckboxes - 10) * 2 == 1){
+            precoingrediente = precoingrediente + 2;
+        }
         else{
-            $('#count-checked-checkboxes').text(countCheckedCheckboxes );
+            $('#count-checked-checkboxes').text(countCheckedCheckboxes);
             $("#count-checked-checkboxes").css({"color": "black"});
             $("#selected-cbx").css({"display": "initial"});
             $("#aviso-ingredientes").css({"display": "none"});
-            
+            precoingrediente = 0;        
         }
+
+          
       });
       
       $('.gordura input').on('change', function() {
@@ -57,10 +57,11 @@ $(document).ready(function(){
         $('#result').text(var1);
      });
 
-
      $( "#btn-enviar" ).click(function() {
 
-        var val2, selec = [ ], var3, var4, add = [];
+        var var1, val2, selec = [ ], var3, var4, add = [];
+
+        
        
             var var1 = $('input[name=gordura]:checked').val(); 
             
@@ -81,14 +82,32 @@ $(document).ready(function(){
             
             $('#testeteste').text(var1 + "" + selec);
             window.location = "https://api.whatsapp.com/send?phone=+5599984850548&text= Olá, este é meu pedido:%0A[--- Passo 1 ---] " + "*"+var1+"* %0A" + "[--- Ingredientes: ---] " + "*"+selec+"* %0A" + " [--- Tipo de Massa: ---] " + "*"+var3+"* %0A" + " [--- Molho: ---] " + "*"+var4+"* %0A" + " [--- Adicionais: ---] " + "*"+add+"* %0A";
-    
-
-
 
       });
 
-
-
       
+
+      var $adicionais = $('#adicionais input[type="checkbox"]');
+      var adicional = $adicionais.change(function(){
+          var countadd = $adicionais.filter(':checked').length;
+            $('#addadd').text(countadd + " Ingrediente(s) Selecionado(s)");
+            if( countadd == 1){
+                precoadd = 232323;
+            }
+            else{
+                precoadd = 23232323;
+            }
+        });
+
+        var precoingrediente = 0;
+        var precoadd = 12;
+        var precototal = precoingrediente;
+
+
+        var teste = $('#ingredientes-container input[type="checkbox"]').length;
+        var teste_teste = teste.filter(':checked').length;
+        $('#total-valor').text(teste);
+
+
 
 });
